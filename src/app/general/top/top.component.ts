a070aca1b74f2028dec74c001eaa8d37d00450d5
Helmer Bergström <hebe2400@student.miun.se>
2025-05-27 17:03:41 +0200

    Gör klart header-sektionen med logga och stilar om.

--- src/app/general/top/top.component.ts
+++ src/app/general/top/top.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './top.component.html',
   styleUrl: './top.component.css'
 })
 export class TopComponent {
-
+    headerImg = '/android-chrome-192x192.png';
 }
