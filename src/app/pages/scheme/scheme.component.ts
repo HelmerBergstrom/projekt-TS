cfb56181b753eecb77e6fa06dd93db12112f7bdd
Helmer Bergström <hebe2400@student.miun.se>
2025-05-30 17:45:42 +0200

    Fixar servicen för ramschema-sidan och tillämpar metoder för att lägga till och uppdatera i array med BehaviorSubject. Laddar kurserna via localstorage och använder if-sats för att kontrollera om data finns och att kurskod stämmer.

--- src/app/pages/scheme/scheme.component.ts
+++ src/app/pages/scheme/scheme.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SchemeService } from '../../services/scheme.service';
+import { Courses } from '../../models/courses';
 
 @Component({
   selector: 'app-scheme',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './scheme.component.html',
   styleUrl: './scheme.component.css'
 })
 export class SchemeComponent {
+  savedCourses: Courses[] = [];
+
+  constructor(private schemeService: SchemeService) {}
+
+  ngOnInit() {
+    this.schemeService.savedCourses.subscribe(courses => {
+      this.savedCourses = courses;
+    })
+  }
 
 }
--- src/app/services/scheme.service.ts
+++ src/app/services/scheme.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Courses } from '../models/courses';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchemeService {
 
+  private savedCoursesSubject = new BehaviorSubject<Courses[]>(this.loadSavedCourses());
+
+  savedCourses = this.savedCoursesSubject.asObservable();
+
   constructor() { }
+
+  private loadSavedCourses(): Courses[] {
+    const saved = localStorage.getItem("savedCourses");
+    return saved ? JSON.parse(saved) : [];
+  };
+
+  updateSavedCourses(course: Courses[]): void {
+    this.savedCoursesSubject.next(course);
+    localStorage.setItem("savedCourses", JSON.stringify(course));
+  };
+
+  addCourse(course: Courses): void {
+    const current = this.savedCoursesSubject.value;
+    if(!current.some(c => c.courseCode === course.courseCode)) {
+      const updated = [...current, course]; 
+      this.updateSavedCourses(updated);
+    };
+  };
 }
