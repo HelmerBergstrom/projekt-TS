74b4e5c61de2980cdd47ee5c6bf0cbe0babcf476
Helmer Bergström <hebe2400@student.miun.se>
2025-06-02 12:17:53 +0200

    Skapar startsida och lägger till stil för den, även en bild som jag lägger till genom TS.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { SchemeComponent } from './pages/scheme/scheme.component';
+import { StartComponent } from './pages/start/start.component';
 
 export const routes: Routes = [
+    { path: 'start', component: StartComponent},
     { path: 'courses', component: CoursesComponent},
     { path: 'scheme', component: SchemeComponent},
     { path: "", redirectTo: "courses", pathMatch: "full"}
