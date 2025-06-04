94775ff8dc44262b185b78642124268e1b4eaa9e
Helmer Bergström <hebe2400@student.miun.se>
2025-06-04 10:29:33 +0200

    Tar bort parametrar i interfacet som inte används.

--- src/app/models/courses.ts
+++ src/app/models/courses.ts
@@ -1,11 +1,8 @@
 export interface Courses {
         "courseCode": string,
         "subjectCode": string,
-        "level": string,
-        "progression": string,
         "courseName": string,
         "points": number,
-        "institutionCode": string,
         "subject": string,
         "syllabus": string
 }
